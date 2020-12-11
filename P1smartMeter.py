da05f1fc740c770a6648793c407e00be5d822790
Jeroen <0982058@hr.nl>
2020-12-11 12:39:18 +0100

    added decoding

--- P1smartMeter.py
+++ P1smartMeter.py
@@ -2,6 +2,8 @@ import sys
 import os
 import serial
 
+
+#settings for setting up serial port
 ser          = serial.Serial()
 ser.baudrate = 9600
 ser.bytesize = serial.EIGHTBITS
@@ -13,14 +15,19 @@ ser.timeout  = 30
 ser.port     = 'com13'
 
 try:
-    ser.open()
+    ser.open()                                          # try opening serial port
 except:
-    sys.exit ("Fout bij het openen van poort %s. "  % ser.name)
+    sys.exit ("error opening the serial port %s. "  % ser.name)
 
-try:
-    data_raw = ser.readline()
-except:
-    sys.exit("Fout bij lezen van poort %s" % ser.name )
-    ser.close()
+for i in range(10):
+    try:
+        data_raw = ser.readline()                       #read the serial port
+    except:
+        sys.exit("error reading serial port%s" % ser.name )
+        ser.close()                                     #close the serial port
 
-print(data_raw)
+    data_line = ''.join(chr(ch & 0x7f) for ch in data_raw).strip()      #decoding stuff
+    print(data_line)
+
+ser.close()
+print("program finished")
